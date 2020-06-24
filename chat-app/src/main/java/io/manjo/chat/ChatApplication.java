203569c9631ed0550d8d7ef6aae55e4fb7cd6f0a
Ivan <ivan.gontarenko@amadeus.com>
2020-06-24 14:27:08 +0300

    Initial

---
+++ chat-app/src/main/java/io/manjo/chat/ChatApplication.java
@@ -0,0 +1,14 @@
+package io.manjo.chat;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class ChatApplication {
+
+    public static void main(String... arguments) {
+        run(ChatApplication.class, arguments);
+    }
+
+}
---
+++ chat-app/src/main/java/io/manjo/chat/controller/ChatController.java
@@ -0,0 +1,31 @@
+package io.manjo.chat.controller;
+
+import io.manjo.chat.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage message) {
+        System.out.println(message);
+        return message;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage message, SimpMessageHeaderAccessor accessor) {
+        accessor.getSessionAttributes().put("username", message.getSender());
+
+        System.out.println(message);
+
+        return message;
+    }
+
+
+}
